21b4efa41eac9d2a9a15435f6ce6b4154eb50a4d
munakoiso <ermilin.c@yandex.ru>
2018-09-28 12:01:16 +0500

    init commit

---
+++ Server.py
@@ -0,0 +1,92 @@
+from bottle import Bottle, run, request, response, abort
+from Tree import Tree, print_tree
+from peewee import DataError, DatabaseError
+
+
+class HttpServer:
+    def __init__(self):
+        self.tree = Tree()
+
+        self.app = Bottle()
+
+        @self.app.get('/')
+        def get():
+
+            try:
+                path = request.GET.get("path")
+                if path is None:
+                    abort(400, "")
+                node_id = self.get_id(path)
+
+                result = self.tree.get_data_of_element(node_id)
+                return result
+            except IndexError:
+                return "Incorrect index"
+            except DatabaseError:
+                return "Incorrect path"
+
+        @self.app.post('/')
+        def post():
+            try:
+                path = request.GET.get("path")
+                name = request.GET.get("name")
+                data = request.GET.get("data")
+
+                if path is None or name is None or data is None:
+                    abort(400, "")
+
+                node_id = self.get_id(path)
+                self.tree.post(name, node_id, data)
+                return "OK"
+            except IndexError:
+                return "Incorrect index"
+            except DataError:
+                return "Incorrect name"
+
+        @self.app.put('/')
+        def put():
+            try:
+                print_tree()
+                path = request.GET.get("path")
+                name = request.GET.get("name")
+                data = request.GET.get("data")
+
+                if path is None or name is None or data is None:
+                    abort(400, "")
+
+                node_id = self.get_id(path)
+                self.tree.put(name, node_id, data)
+                return "OK"
+
+            except IndexError:
+                return "Incorrect index"
+            except DataError:
+                return "Incorrect name"
+
+        @self.app.delete('/')
+        def delete():
+            try:
+                path = request.GET.get("path")
+                node_id = self.get_id(path)
+                self.tree.delete(node_id)
+
+                if path is None:
+                    abort(400, "")
+
+                return "OK"
+            except IndexError:
+                return "Incorrect index"
+            except DataError:
+                return "Incorrect name"
+
+
+    def get_id(self, path_str):
+        path = path_str.split('/')
+        return self.tree.get_element_id(path)
+
+
+if __name__ == '__main__':
+    server = HttpServer()
+    host = 'localhost'
+    port = 8080
+    run(server.app, host=host, port=port)
---
+++ Tree.py
@@ -0,0 +1,144 @@
+import re
+from peewee import SqliteDatabase, CharField, IntegerField, Model, ForeignKeyField, DataError, DatabaseError
+
+db = SqliteDatabase('Tree.db')
+
+
+def print_tree():
+    for e in Node.select():
+        print(e.name)
+    print('___')
+    for e in Parent.select():
+        print(e.parent_id.get_id(), e.children_id.get_id())
+    print('___')
+
+
+class Node(Model):
+
+    name = CharField()
+    data = CharField(null=True)
+
+    @staticmethod
+    def is_contain_id(node_id):
+        return len(Node.select().where(Node.id == node_id)) != 0
+
+    class Meta:
+        database = db
+
+
+class Parent(Model):
+    children_id = ForeignKeyField(model=Node)
+    parent_id = ForeignKeyField(model=Node)
+
+    @staticmethod
+    def get_parent_id(children_id):
+        print(children_id)
+        if not Node.is_contain_id(children_id):
+            raise DataError('Id not exist in database')
+
+        parent = Parent.select().where(Parent.children_id.get_id() == children_id).get()
+        return parent.parent_id.get_id()
+
+    @staticmethod
+    def get_childrens_id(parent_id):
+        if not Node.is_contain_id(parent_id):
+            raise DataError('Id not exist in database')
+        result = []
+        for e in Parent.select().where(Parent.parent_id.get_id() == parent_id):
+            result.append(e.get_id())
+        return result
+
+    class Meta:
+        database = db
+
+
+class Tree:
+    def __init__(self):
+
+        Node.create_table(safe=True)
+        Parent.create_table(safe=True)
+        if not Node.is_contain_id(0):
+            Node.create(name='', id=0)
+            Node.create(name='.')
+            Parent.create(parent_id=0, children_id=1)
+
+    def get_element_id(self, names):
+        current_id = 0
+        for name in names:
+            is_children_exist = False
+            for children_id in Parent.get_childrens_id(current_id):
+                if Node.get_by_id(children_id).name == name:
+                    current_id = children_id
+                    is_children_exist = True
+                    break
+            if not is_children_exist:
+                raise DatabaseError('Incorrect path')
+        return current_id
+
+    def get_elements(self, node_id):
+        elements = []
+        if not Node.is_contain_id(node_id):
+            raise IndexError('Id not exist in database')
+        for children_id in Parent.get_childrens_id(node_id):
+            elements.append(Node.get_by_id(children_id))
+        return elements
+
+    def get_data_of_element(self, node_id):
+        if not Node.is_contain_id(node_id):
+            raise IndexError('Id not exist in database')
+        node = Node.get_by_id(node_id)
+        return "{} - {}".format(node.name, node.data)
+
+    def put(self, name, node_id=0, data=None):
+        if not Node.is_contain_id(node_id):
+            raise IndexError('Id not exist in database')
+        parent = Node.get_by_id(node_id)
+        if not Tree.is_name_correct(name, node_id):
+            raise DataError("Name isn't corrent")
+        parent_id = parent.get_id()
+        new_element = Node.create(name=name, data=data)
+        new_id = new_element.get_id()
+        Parent.create(children_id=new_id, parent_id=parent_id)
+
+    def post(self, name, node_id, data=None):
+        if not Node.is_contain_id(node_id):
+            raise IndexError('Id not exist in database')
+        element = Node.get_by_id(node_id)
+        if not Tree.is_name_correct(name, Parent.get_parent_id(node_id), element.id):
+            raise DataError("Name isn't correct")
+
+        element.data = data
+        element.name = name
+        element.save()
+
+    def delete(self, node_id, delete_prev_edges=True):
+        edges_id = []
+        for edge in Parent.select().where(Parent.parent_id.get_id() == node_id):
+            children_id = edge.children_id.get_id()
+            self.delete(children_id, False)
+            edges_id.append(edge.get_id())
+
+        Node.delete_by_id(node_id)
+        if delete_prev_edges:
+            for edge in Parent.select().where(Parent.children_id.get_id() == node_id):
+                edges_id.append(edge.get_id())
+        for edge_id in edges_id:
+            Parent.delete_by_id(edge_id)
+
+    @staticmethod
+    def is_name_correct(name, parent_id, current_id=None):
+        for children_id in Parent.get_childrens_id(parent_id):
+            if Node.get_by_id(children_id).name == name:
+                if children_id != current_id:
+                    return False
+        return re.match(r'([a-zA-Zа-яА-Я]+ )*[a-zA-Zа-яА-Я]+', name) and \
+               not Tree.is_name_in_branch(name, parent_id)
+
+    @staticmethod
+    def is_name_in_branch(name, parent_id):
+
+        if parent_id == 0:
+            return False
+
+        return Node.get_by_id(parent_id).name == name or \
+               Tree.is_name_in_branch(name, Parent.get_parent_id(parent_id))
